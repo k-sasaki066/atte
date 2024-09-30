b784d3bfe658d5747fbdc1f1784685291a47b6f4
k-sasaki <k.sasaki.bluesky@gmail.com>
2024-09-30 12:53:48 +0900

    indexファイル編集

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+    <div class="content__inner">
+        <h2 class="content__header">テストさんお疲れ様です!</h2>
+        <form class="attendance-form__group" action="" method="">
+            <button class="work-form__submit" type="submit" name="">
+                勤務開始
+            </button>
+
+            <button class="work-form__submit" type="submit" name="">
+                勤務終了
+            </button>
+
+            <button class="work-form__submit" type="submit" name="">
+                休憩開始
+            </button>
+
+            <button class="work-form__submit" type="submit" name="">
+                休憩終了
+            </button>
+        </form>
+    </div>
+@endsection
+

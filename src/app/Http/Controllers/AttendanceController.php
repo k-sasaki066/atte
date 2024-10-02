4293a3cf4ec68e94fbc6697e01b43f3e6f2af9ad
k-sasaki <k.sasaki.bluesky@gmail.com>
2024-10-02 10:55:02 +0900

    ダミーデータ作成

--- src/app/Http/Controllers/AttendanceController.php
+++ src/app/Http/Controllers/AttendanceController.php
@@ -24,6 +24,7 @@ class AttendanceController extends Controller
         }else {
             $status = Auth::user()->status;
         }
+        // dd( $status);
 
         return view('index')->with('status',$status);
     }
